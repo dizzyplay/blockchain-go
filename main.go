1713a9207f50c5eb04c30e49b6524db6cb3a69ab
dizzyplay <vovoboss@gmail.com>
2021-10-24 01:31:00 +0900

    ok

--- main.go
+++ main.go
@@ -1,1 +1,45 @@
-package blockchain_go
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+type blockchain struct {
+	blocks []block
+}
+
+type block struct {
+	data     string
+	hash     string
+	prevHash string
+}
+
+func (b *blockchain) getLastHash() string {
+	if len(b.blocks) > 0 {
+		return b.blocks[len(b.blocks)-1].hash
+	}
+	return ""
+}
+
+func (b *blockchain) addBlock(data string) {
+	newBlock := block{data, "", b.getLastHash()}
+	newBlock.hash = fmt.Sprintf("%x", sha256.Sum256([]byte(newBlock.data+newBlock.prevHash)))
+	b.blocks = append(b.blocks, newBlock)
+}
+
+func (b *blockchain) listBlocks() {
+	for _, block := range b.blocks {
+		fmt.Printf("Data: %s \n ", block.data)
+		fmt.Printf("hash: %s \n ", block.hash)
+		fmt.Printf("prevHash: %s \n ", block.prevHash)
+	}
+}
+
+func main() {
+	chain := blockchain{}
+	chain.addBlock("one")
+	chain.addBlock("two")
+	chain.addBlock("three")
+	chain.listBlocks()
+}
