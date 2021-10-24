234d8b77affef7b6850335d97e40962f042751fe
dizzyplay <vovoboss@gmail.com>
2021-10-24 22:24:18 +0900

    ok

--- main.go
+++ main.go
@@ -8,19 +8,33 @@ import (
 	"net/http"
 )
 
-const port string = ":4000"
+const (
+	port        string = ":4000"
+	templateDir string = "templates/"
+)
 
 type HomeData struct {
 	PageTitle string
-	Blocks []*blockchain.Block
+	Blocks    []*blockchain.Block
+}
+
+var templates *template.Template
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	data := HomeData{"Home", blockchain.GetBlockChain().AllBlocks()}
+	templates.ExecuteTemplate(rw, "home", data)
+}
+
+func add(rw http.ResponseWriter, r * http.Request) {
+	fmt.Println(r)
+	templates.ExecuteTemplate(rw, "add", nil)
 }
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		tmpl := template.Must(template.ParseFiles("templates/pages/home.gohtml"))
-		data := HomeData{"Home", blockchain.GetBlockChain().AllBlocks() }
-		tmpl.Execute(writer, data)
-	})
-	fmt.Printf("http://localhost%s",port)
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
+	fmt.Printf("http://localhost%s", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
