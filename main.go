949a08f7104213533c0716a2728ce373808e210f
dizzyplay <vovoboss@gmail.com>
2021-10-25 00:39:35 +0900

    josn

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "github.com/dizzyplay/blockchain-go/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(data)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
