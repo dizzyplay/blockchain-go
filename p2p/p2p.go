57c0e275fc103f504945a4699c13cabd6b369bae
dizzyplay <vovoboss@gmail.com>
2021-11-17 00:22:27 +0900

    chat

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -7,11 +7,24 @@ import (
 )
 
 var upgrader = websocket.Upgrader{}
+var conns []*websocket.Conn
 
 func Upgrade(rw http.ResponseWriter, r *http.Request){
 	upgrader.CheckOrigin = func (r *http.Request)bool {
 		return true
 	}
-	_, err := upgrader.Upgrade(rw, r, nil)
+	conn, err := upgrader.Upgrade(rw, r, nil)
+	conns = append(conns, conn)
 	utils.HandleError(err)
+	for {
+		_,p,err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		for _, aConn := range conns {
+			if aConn != conn {
+				utils.HandleError(aConn.WriteMessage(websocket.TextMessage, p))
+			}
+		}
+	}
 }
